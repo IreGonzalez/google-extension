0f6fb0cc980e4b057bb5d8a966c79b0ec13e6edf
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-11-23 11:50:01 +0100

    add image function to fix

---
+++ src/js/background.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const backgroundImg = document.getElementById('js-background').src = './assets/images/Bg-1.jpg';
+
+function background() {
+    const bg = [
+        'Bg-1.jpg',
+        'Bg-2.jpg',
+        'Bg-3.jpg',
+        'Bg-4.jpg',
+        'Bg-5.jpg',
+        'Bg-6.jpg',
+        'Bg-7.jpg',
+        'Bg-8.jpg',
+        'Bg-9.jpg',
+        'Bg-10.jpg',
+        'Bg-11.jpg'
+    ]
+    const randomNumber = Math.ceil(Math.random() * bg.length)
+    console.log(randomNumber);
+    const randomBg = bg.slice(randomNumber, randomNumber + 1);
+    const bgImage = randomBg[0];
+    console.log(bgImage);
+    console.log(backgroundImg);
+    console.log(`./assets/images/${bgImage}`);
+    backgroundImg.innerHTML = `./assets/images/${bgImage}`;
+
+};
+background();
