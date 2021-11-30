c054b65cb10a9a58f10b20827bb65ce099fa785c
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-11-30 19:14:34 +0100

    done styles

--- src/js/goodMorning.js
+++ src/js/goodMorning.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let inputName = document.querySelector('.js-inputName');
-let nameHTML = document.querySelector('.js-name');
 const submit = document.querySelector('.js-submit');
 
 const handleMorning = (ev) => {
+    const inputName = document.querySelector('.js-inputName');
+    const nameHTML = document.querySelector('.js-name');
     ev.preventDefault();
     nameHTML.innerHTML = inputName.value;
 }
