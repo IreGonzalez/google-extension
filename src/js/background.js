128e2bf3466acd6111ada393c63270ae2d008cc9
IreGonzalez <irenegonzalezpinilla@gmail.com>
2022-11-23 21:02:08 +0100

    add personal images

--- src/js/background.js
+++ src/js/background.js
@@ -4,16 +4,17 @@ const backgroundImg = document.querySelector('.js-background');
 
 function background() {
     const bg = [
-        'https://live.staticflickr.com/65535/51591456521_08d2053421_k.jpg',
-        'https://live.staticflickr.com/1647/25882970004_b5bbed15fc_k.jpg',
-        'https://live.staticflickr.com/65535/51630395178_bc42132bbf_h.jpg',
-        'https://live.staticflickr.com/65535/51709922156_e11ec9b0d5_4k.jpg',
-        'https://live.staticflickr.com/65535/51628944483_fd5469b4aa_k.jpg',
-        'https://live.staticflickr.com/65535/51654618313_6da46ca69a_k.jpg',
-        'https://live.staticflickr.com/65535/51625321315_f0fef92343_k.jpg',
-        'https://live.staticflickr.com/65535/51620547363_8a9054961e_k.jpg',
-        'https://live.staticflickr.com/65535/51338462438_6801a04ce9_k.jpg',
-        'https://live.staticflickr.com/65535/49329061008_cf8ea80d4f_k.jpg',
+        '../assets/images/Bg-1.jpg',
+        '../assets/images/Bg-2.jpg',
+        '../assets/images/Bg-3.jpg',
+        '../assets/images/Bg-4.jpg',
+        '../assets/images/Bg-5.jpg',
+        '../assets/images/Bg-6.jpg',
+        '../assets/images/Bg-7.jpg',
+        '../assets/images/Bg-8.jpg',
+        '../assets/images/Bg-9.jpg',
+        '../assets/images/Bg-10.jpg',
+        '../assets/images/Bg-11.jpg'
     ]
     const randomNumber = Math.ceil(Math.random() * bg.length)
     const randomBg = bg.slice(randomNumber - 1, randomNumber);
