8c8ede84caf87cf6fad8e5b5e8ce1482b994808d
IreGonzalez <irenegonzalezpinilla@gmail.com>
2022-11-23 21:47:27 +0100

    actualized route

--- src/js/background.js
+++ src/js/background.js
@@ -4,17 +4,17 @@ const backgroundImg = document.querySelector('.js-background');
 
 function background() {
     const bg = [
-        '../assets/images/Bg-1.jpg',
-        '../assets/images/Bg-2.jpg',
-        '../assets/images/Bg-3.jpg',
-        '../assets/images/Bg-4.jpg',
-        '../assets/images/Bg-5.jpg',
-        '../assets/images/Bg-6.jpg',
-        '../assets/images/Bg-7.jpg',
-        '../assets/images/Bg-8.jpg',
-        '../assets/images/Bg-9.jpg',
-        '../assets/images/Bg-10.jpg',
-        '../assets/images/Bg-11.jpg'
+        './assets/images/Bg-1.jpg',
+        './assets/images/Bg-2.jpg',
+        './assets/images/Bg-3.jpg',
+        './assets/images/Bg-4.jpg',
+        './assets/images/Bg-5.jpg',
+        './assets/images/Bg-6.jpg',
+        './assets/images/Bg-7.jpg',
+        './assets/images/Bg-8.jpg',
+        './assets/images/Bg-9.jpg',
+        './assets/images/Bg-10.jpg',
+        './assets/images/Bg-11.jpg'
     ]
     const randomNumber = Math.ceil(Math.random() * bg.length)
     const randomBg = bg.slice(randomNumber - 1, randomNumber);
